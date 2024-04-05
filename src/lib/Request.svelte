645391c6b9c558ffb815d47f1b869cb70a2c0691
Németh Botond <nemethbotond.sopron@gmail.com>
2024-04-05 12:43:44 +0200

    CodeOwners
    Signed-off-by: Németh Botond <nemethbotond.sopron@gmail.com>

---
+++ src/lib/Request.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { Avatar } from "@skeletonlabs/skeleton";
+    import SocialIcons from '@rodneylab/svelte-social-icons';
+    import avatar from "../assets/avatar.png";
+
+    const requestDate = new Date('2024/04/05');
+    const today = new Date();
+
+    const done = false
+
+    function calculateDaysBetween(date1: Date, date2: Date) {
+        const oneDay = 1000 * 60 * 60 * 24;
+        const diffInTime = Math.abs(date2.getTime() - date1.getTime());
+        return Math.ceil(diffInTime / oneDay);
+    }
+</script>
+
+<div class="container min-h-screen mx-auto flex justify-center items-center">
+    <div class="space-y-10">
+        <h1 class="h1">Can you draw my Profile 🥺👉👈</h1>
+        <div class="flex justify-center items-center">
+            <Avatar src={avatar} width="w-40" rounded="rounded-full"/>
+        </div>
+        <a class="block card p-4 text-center text-2xl" href="/">Day {calculateDaysBetween(today, requestDate)} of asking to draw me</a>
+        <div class="flex justify-center items-center">
+            <a href="https://www.instagram.com/nemeth1375/">
+                <SocialIcons network="instagram" fgColor="#eeeeee" bgColor="#2e395a"/>
+            </a> 
+        </div>
+    </div>
+</div>
---
+++ src/lib/Thanks.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { Confetti } from "svelte-confetti"
+</script>
+
+<div class="fixed top-[-50px] left-0 h-screen w-screen flex justify-center overflow-hidden pointer-events-none">
+    <Confetti x={[-5, 5]} y={[0, 0.1]} delay={[500, 2000]} infinite duration={5000} amount={200} fallDistance="100vh" />
+</div>
+
+<div class="flex min-h-screen justify-center items-center">
+    <h1 class="h1">Thanks 😊</h1>
+</div>
